de927c9e59b6a044143bba04680322df1657cf00
samdan <samdan.dev@gmail.com>
2020-12-22 16:25:42 +0800

    changed checkout into payment

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
-import React from 'react';
-import { Button, Card, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Nav,
+  Navbar,
+  Row,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import img1 from './assets/product-1.jpg';
-import img2 from './assets/product-2.jpg';
+import img1 from "./assets/product-1.jpg";
+import img2 from "./assets/product-2.jpg";
 
-const code = 'B3D3DF06DDA15B44E0532A64A8C0DF61';
-const tokenUrl = 'https://test.hipay.mn/v2/auth/token1';
+const code = "B3D3DF06DDA15B44E0532A64A8C0DF61";
+const tokenUrl = "https://test.hipay.mn/v2/auth/token1";
 // const profileUrl = 'https://test.hipay.mn/v2/auth/token1';
 
 function App() {
-  const [products] = React.useState([{
-    name: 'T-Shirt 1',
-    price: "15'000",
-    image: img1
-  }, {
-    name: 'T-Shirt 2',
-    price: "20'000",
-    image: img2
-  }]);
+  const [products] = React.useState([
+    {
+      name: "T-Shirt 1",
+      price: "15'000",
+      image: img1,
+    },
+    {
+      name: "T-Shirt 2",
+      price: "20'000",
+      image: img2,
+    },
+  ]);
   // const [token, setToken] = React.useState(null);
-  const [profile] = React.useState({ name: 'hi' });
+  const [profile] = React.useState({ name: "hi" });
 
   const getToken = React.useCallback(async () => {
     const tokenResponse = await fetch(tokenUrl, {
-      mode: 'cors',
-      method: 'POST',
+      mode: "cors",
+      method: "POST",
       body: {
         client_id: "songo.mn",
         client_secret: "NEgxQ0x4bTJ0WXJPOW1JeA",
         redirect_uri: "https://google.com",
         code,
-        grant_type: "authorization_code"
-      }
+        grant_type: "authorization_code",
+      },
     });
     const data = await tokenResponse.json();
-    if (data.code === '1') {
-      console.log(data)
+    if (data.code === "1") {
+      console.log(data);
     } else {
-      console.error('Токен авхад алдаа гарлаа: ', data.details)
+      console.error("Токен авхад алдаа гарлаа: ", data.details);
     }
   }, []);
 
   React.useEffect(() => {
-    console.log(window.hpsCheckout);
     getToken();
   }, [getToken]);
 
@@ -64,23 +74,27 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Container >
-        {window.testMessage + '11111111111111'}
+      <Container>
         <Row>
-          {
-            products.map((product, i) => (
-              <Col key={i} xs={6} style={{ paddingTop: 16 }}>
-                <Card >
-                  <Card.Img variant="top" src={product.image} />
-                  <Card.Body>
-                    <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>₮{product.price}</Card.Text>
-                    <Button onClick={() => { window.hpsCheckout(15000, 'songo.mn') }} variant="primary">Авах</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))
-          }
+          {products.map((product, i) => (
+            <Col key={i} xs={6} style={{ paddingTop: 16 }}>
+              <Card>
+                <Card.Img variant="top" src={product.image} />
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>₮{product.price}</Card.Text>
+                  <Button
+                    onClick={() => {
+                      window.hpsPayment("B709BAAC70025785E0532A64A8C0BF31");
+                    }}
+                    variant="primary"
+                  >
+                    Авах
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>
     </>
